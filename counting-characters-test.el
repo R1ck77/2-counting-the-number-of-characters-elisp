4b00506c1d1896163a6a339bba2b528e6e8971e3
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-06 18:21:17 +0200

    Messages honor the user preferences

--- counting-characters-test.el
+++ counting-characters-test.el
@@ -5,31 +5,49 @@
 
 (defconst some-text "Input by user")
 
-(describe "count-characters"
-  (it "prompts for a string using the correct text"
-    (spy-on 'read-string :and-return-value "Something")
-    (with-temp-buffer
-      (count-characters))
-    (expect 'read-string
-            :to-have-been-called-with "What is the input string? "))
-  (it "writes the correct length for the input in the current buffer"
-    (let ((result)
-          (prev-text "PREVIOUS TEXT"))
+(describe "all tests"
+  (describe "count-characters"
+    (it "prompts for a string using the correct text"
+      (spy-on 'read-string :and-return-value "Something")
       (with-temp-buffer
-        (insert prev-text)
-        (spy-on 'read-string :and-return-value some-text)
+        (count-characters))
+      (expect 'read-string
+              :to-have-been-called-with "What is the input string? "))
+    (it "writes the correct length for the input in the current buffer"
+      (let ((result)
+            (prev-text "PREVIOUS TEXT"))
+        (with-temp-buffer
+          (insert prev-text)
+          (spy-on 'read-string :and-return-value some-text)
+          (count-characters)
+          (setq result (buffer-substring-no-properties (point-min) (point-max))))
+        (expect result
+                :to-equal (format "%s%s has %d characters" prev-text some-text (length prev-text)))))
+    (it "prompts the user to write something if nothins enters nothing"
+      (lexical-let ((empty-calls 5)
+                    (current-call 0))
+        (spy-on 'read-string :and-call-fake (lambda (x)
+                                              (setq current-call (+ 1 current-call))
+                                              (if (<= current-call empty-calls)
+                                                  ""
+                                                "Dave")))
+        (spy-on 'message)
+        (spy-on 'sit-for)
         (count-characters)
-        (setq result (buffer-substring-no-properties (point-min) (point-max))))
-      (expect result
-              :to-equal (format "%s%s has %d characters" prev-text some-text (length prev-text)))))
-  (it "prompts the user to write something if nothins enters nothing"
-    (lexical-let ((empty-calls 2)
-                  (current-call 0))
-      (spy-on 'read-string :and-call-fake (lambda (x)
-                                            (setq current-call (+ 1 current-call))
-                                            (if (<= current-call empty-calls)
-                                                ""
-                                              "Dave")))
-      (spy-on 'message)
-      (count-characters)
-      (expect 'message :to-have-been-called-times 2))))
+        (expect 'message :to-have-been-called-times 5)
+        (expect 'sit-for :to-have-been-called-times 5))))
+  (describe "cc--send-messages-and-wait"
+    (it "honors the minibuffer-message-timeout, when set"
+      (let ((minibuffer-message-timeout 12))
+        (spy-on 'sit-for)
+        (spy-on 'message)
+        (cc--send-message-and-wait some-text)
+        (expect 'sit-for
+                :to-have-been-called-with 12)))
+    (it "returns 1\" when no timeout is specified by the user"
+      (let ((minibuffer-message-timeout "nope"))
+        (spy-on 'sit-for)
+        (spy-on 'message)
+        (cc--send-message-and-wait some-text)
+        (expect 'sit-for
+                :to-have-been-called-with 1)))))
--- counting-characters.el
+++ counting-characters.el
@@ -1,8 +1,21 @@
 (defconst prompt-text "What is the input string? ")
 (defconst output-format-string "%s has %d characters")
 
+(defconst cc--fallback-minibuffer-message-timeout 1
+  "Timeout to wait for a message to disappear when no timeout is specified by the user")
+
 (defun cc--emptyp (text)
   (= 0 (length text)))
+
+(defun cc--get-minibuffer-message-timeout ()
+    (let ((timeout minibuffer-message-timeout))
+      (if (numberp timeout)
+          timeout
+        cc--fallback-minibuffer-message-timeout)))
+
+(defun cc--send-message-and-wait (text)
+  (message text)
+  (sit-for (cc--get-minibuffer-message-timeout)))
 
 (defun count-characters ()
   (interactive)
@@ -10,9 +23,7 @@
     (while (cc--emptyp text)
       (setq text (read-string prompt-text))
       (if (cc--emptyp text)
-          (progn
-            (message "Enter some text")
-            (sit-for 1))
+          (cc--send-message-and-wait "Enter some text")
         (insert (format output-format-string text (length text)))))))
 
 (provide 'counting-characters)
