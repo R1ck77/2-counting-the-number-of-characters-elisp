d9eb1e1b8033eeb8c2c5928468fbda8b5e0c9252
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-06 22:41:14 +0200

    Real-time version. Not test covered, needs refactoring
    Really convoluted feedback-providing version of the count characters,
    as specified by the challenge.
    
    Testing this may be a real b...h due to all the hooks involved.

--- counting-characters.el
+++ counting-characters.el
@@ -1,3 +1,5 @@
+(require 'cl)
+
 (defconst prompt-text "What is the input string? ")
 (defconst output-format-string "%s has %d characters")
 
@@ -17,23 +19,44 @@
   (message text)
   (sit-for (cc--get-minibuffer-message-timeout)))
 
-(defun cc--after-change-function-generator (buffer)
-  (lexical-let ((buffer buffer))
+(defun cc--remove-insertion-selection (insertion-beginning)
+  (let ((old-insertion-end (function-get 'cc--minibuffer-setup-hook 'insertion-end)))
+    (when old-insertion-end
+      (delete-region insertion-beginning old-insertion-end)
+      (function-put 'cc--minibuffer-setup-hook 'insertion-end nil))))
+
+(defun cc--insert-text (insertion-start text)
+  (cc--remove-insertion-selection insertion-start)
+  (goto-char insertion-start)
+  (insert (format output-format-string text (length text))))
+
+(defun cc--after-change-function-generator (buffer insertion-beginning)
+  (lexical-let ((buffer buffer)
+                (insertion-beginning insertion-beginning))
     (lambda (beginning end prev-length)
-      (insert (buffer-substring-no-properties beginning end))
-      (insert "\n"))))
+      (let ((insertion-text (minibuffer-contents)))
+        (with-current-buffer buffer
+          (cc--insert-text insertion-beginning insertion-text)
+          (function-put 'cc--minibuffer-setup-hook 'insertion-end (point)))))))
 
 (defun cc--minibuffer-setup-hook ()
   (add-hook 'after-change-functions
-            (cc--after-change-function-generator (function-get 'cc--minibuffer-setup-hook 'buffer))
+            (cc--after-change-function-generator (function-get 'cc--minibuffer-setup-hook 'buffer)
+                                                 (function-get 'cc--minibuffer-setup-hook 'insertion-beginning))
             nil t))
 
 (defun cc--minibuffer-exit-hook ()
   (remove-hook 'after-change-functions t)
   (cc--remove-hooks))
 
+(defun cc--clear-contextual-properties ()
+  (function-put 'cc--minibuffer-setup-hook 'buffer nil)
+  (function-put 'cc--minibuffer-setup-hook 'insertion-beginning nil)
+  (function-put 'cc--minibuffer-setup-hook 'insertion-end nil))
+
 (defun cc--add-hooks ()
   (function-put 'cc--minibuffer-setup-hook 'buffer (current-buffer))
+  (function-put 'cc--minibuffer-setup-hook 'insertion-beginning (point))
   (add-hook 'minibuffer-setup-hook 'cc--minibuffer-setup-hook)
   (add-hook 'minibuffer-exit-hook 'cc--minibuffer-exit-hook))
 
@@ -47,11 +70,13 @@
 
 (defun count-characters ()
   (interactive)
-  (let ((text ""))
+  (cc--clear-contextual-properties)
+  (let ((text "")
+        (insertion-start (point)))
     (while (cc--emptyp text)
       (setq text (cc--hooked-read-string))
       (if (cc--emptyp text)
           (cc--send-message-and-wait "Enter some text")
-        (insert (format output-format-string text (length text)))))))
+        (cc--insert-text insertion-start text)))))
 
 (provide 'counting-characters)
